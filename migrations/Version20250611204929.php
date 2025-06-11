77e707e33f4557454a740506123eaa1c7d4ef1a9
Scott Anderson <simple.scott.94@googlemail.com>
2025-06-11 22:09:36 +0100

    main - Add `DispatchNote` entity

---
+++ migrations/Version20250611204929.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250611204929 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE dispatch_note (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, shopware_order_number VARCHAR(100) NOT NULL, customer_delivery_address CLOB NOT NULL, customer_phone_number VARCHAR(100) DEFAULT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE dispatch_note');
+    }
+}
