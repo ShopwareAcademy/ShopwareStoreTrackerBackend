08333b4bb2ee9d2a343f2de939f52d28d54c95e3
Beytullah Cevik <b.cevik@shopware.com>
2025-12-19 11:22:05 +0100

    Fix: DispatchNote, type of customerdeliveryaddress

--- src/Entity/DispatchNote.php
+++ src/Entity/DispatchNote.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\DispatchNoteRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DispatchNoteRepository::class)]
@@ -16,7 +17,7 @@ class DispatchNote
     #[ORM\Column(length: 100)]
     private ?string $shopwareOrderNumber = null;
 
-    #[ORM\Column]
+    #[ORM\Column(type: Types::JSON)]
     private array $customerDeliveryAddress = [];
 
     #[ORM\Column(length: 100, nullable: true)]
